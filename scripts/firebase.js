3d0efa1e6c7a97c538d7682e8a4856babda8baef
Paulius Savickas <pauliussavickas18@gmail.com>
2023-07-17 19:40:52 +0300

    commit

---
+++ scripts/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8H0WYL6_o9l50yALOYtY5pmJvDr5GmKY",
+  authDomain: "skelbimas-7657f.firebaseapp.com",
+  databaseURL: "https://skelbimas-7657f-default-rtdb.firebaseio.com",
+  projectId: "skelbimas-7657f",
+  storageBucket: "skelbimas-7657f.appspot.com",
+  messagingSenderId: "978781892959",
+  appId: "1:978781892959:web:bc6893c8d7ac10eece46ea"
+};
